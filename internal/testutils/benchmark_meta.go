a8d0f04e028b2bc71075a7bec7a9988fd6d16da7
Gottfried Herold <gottfried.herold@rub.de>
2022-11-08 17:41:33 +0100

    Added functionality and tests for precomputed table creation.

--- internal/testutils/benchmark_meta.go
+++ internal/testutils/benchmark_meta.go
@@ -1,23 +1,69 @@
 package testutils
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
 )
 
 type PrecomputedCacheEntry[MapKey comparable, ElementType any] struct {
-	rng      *rand.Rand
-	elements []ElementType
-	key      MapKey
-	mut      sync.RWMutex
+	rng        *rand.Rand
+	elements   []ElementType
+	key        MapKey
+	entryMutex sync.RWMutex
 }
 
 type PrecomputedCache[MapKey comparable, ElementType any] struct {
-	mut                sync.RWMutex
+	tableMutex         sync.RWMutex
 	data               map[MapKey]*PrecomputedCacheEntry[MapKey, ElementType]
 	createRandFromSeed func(key MapKey) *rand.Rand
-	copyFun            func(ElementType) ElementType
 	creationFun        func(*rand.Rand, MapKey) ElementType
+	copyFun            func(ElementType) ElementType
+}
+
+func MakePrecomputedCache[MapKey comparable, ElementType any](createRandFromSeed func(MapKey) *rand.Rand, creationFun func(*rand.Rand, MapKey) ElementType, copyFun func(ElementType) ElementType) (ret PrecomputedCache[MapKey, ElementType]) {
+	ret.data = make(map[MapKey]*PrecomputedCacheEntry[MapKey, ElementType])
+
+	// If createRandFromSeed is nil, we populate it with a valid function that just returns nil.
+	// USING the created *rand.Rand will likely fail.
+	// This is needed to make prepopulated PrecomputedCache's work, where the output rng seed is likely never used anyway.
+	if createRandFromSeed == nil {
+		ret.createRandFromSeed = func(MapKey) *rand.Rand { return nil }
+	} else {
+		ret.createRandFromSeed = createRandFromSeed
+	}
+
+	// creationFun may be nil. In this case, automatic extension of the precomputed caches will fail and only prepopulated one are meaningful.
+	ret.creationFun = creationFun
+
+	// nil copyFuns get replaced by a trivial one that just naively copies the element.
+	// The only reason why we even need copyFun is when ElementType is, say, a pointer (possibly in an interface)
+	if copyFun == nil {
+		ret.copyFun = func(in ElementType) ElementType { return in }
+	} else {
+		ret.copyFun = copyFun
+	}
+	return
+}
+
+func (pc *PrecomputedCache[MapKey, ElementType]) PrePopulateCache(key MapKey, cacheEntries []ElementType) {
+	copyFun := pc.copyFun
+	pc.tableMutex.Lock()
+	page, ok := pc.data[key]
+	if ok {
+		pc.tableMutex.Unlock()
+		panic(fmt.Errorf("trying to populate cache under key %v, which already exists", key))
+	}
+	Assert(page == nil)
+	page = pc.newCacheEntry(key)
+	pc.data[key] = page
+	page.entryMutex.Lock() // cannot block
+	pc.tableMutex.Unlock()
+	for _, entry := range cacheEntries {
+		page.elements = append(page.elements, copyFun(entry))
+	}
+	page.entryMutex.Unlock()
+
 }
 
 func (pc *PrecomputedCache[MapKey, ElementType]) GetElements(key MapKey, amount int) (ret []ElementType) {
@@ -25,32 +71,32 @@ func (pc *PrecomputedCache[MapKey, ElementType]) GetElements(key MapKey, amount 
 		return make([]ElementType, 0)
 	}
 	// writing down the types on purpose
-	var entry *PrecomputedCacheEntry[MapKey, ElementType]
+	var page *PrecomputedCacheEntry[MapKey, ElementType]
 	var ok bool
 
 	// Read existing entry from cache
-	pc.mut.RLock()
-	entry, ok = pc.data[key]
-	pc.mut.RUnlock()
+	pc.tableMutex.RLock()
+	page, ok = pc.data[key]
+	pc.tableMutex.RUnlock()
 
 	// If the key was not in the map, we need to create it.
 	if !ok {
 		// create new entry
-		entry = pc.newCacheEntry(key)
+		page = pc.newCacheEntry(key)
 
 		// try to write it to the map:
-		pc.mut.Lock()
-		altentry, ok := pc.data[key] // check again if data was present
+		pc.tableMutex.Lock()
+		existingPage, ok := pc.data[key] // check again if data was present
 		if ok {
-			entry = altentry // if some other goroutine alread filled the entry, discard our work and use that. The first goroutine to write "wins".
+			page = existingPage // if some other goroutine alread filled the entry, discard our work and use that. The first goroutine to write "wins".
 		} else {
-			pc.data[key] = entry
+			pc.data[key] = page
 		}
-		pc.mut.Unlock()
+		pc.tableMutex.Unlock()
 	}
 	// in any case, we now have a valid entry. Get ret from it.
 
-	return entry.getElements(amount, pc)
+	return page.getElements(amount, pc)
 }
 
 func (pc *PrecomputedCache[MapKey, ElementType]) newCacheEntry(key MapKey) (ret *PrecomputedCacheEntry[MapKey, ElementType]) {
@@ -70,48 +116,31 @@ func (pc *PrecomputedCacheEntry[MapKey, ElementType]) getElements(
 	}
 
 	copyFun := source.copyFun
-	if copyFun == nil {
-		copyFun = func(in ElementType) ElementType {
-			return in
-		}
-	}
 
-	pc.mut.RLock()
+	pc.entryMutex.RLock()
 	currentLen := len(pc.elements)
 
 	if currentLen >= amount {
 		for i := 0; i < amount; i++ {
 			ret[i] = copyFun(pc.elements[i])
 		}
-		pc.mut.RUnlock()
+		pc.entryMutex.RUnlock()
 		return
 	}
 
 	// We need to create more entries
-	pc.mut.RUnlock()
+	pc.entryMutex.RUnlock()
 	// sync.RWMutex cannot atomically upgrade read-locks to rw-locks.
 	creationFun := source.creationFun
-	pc.mut.Lock()
-	currentLen = len(pc.elements) // reload due to the above non-atomicity
-	for currentLen < amount {
+	pc.entryMutex.Lock()
+	for len(pc.elements) < amount {
 		pc.elements = append(pc.elements, creationFun(pc.rng, pc.key))
 	}
-	pc.mut.Unlock()
-	pc.mut.RLock()
+	pc.entryMutex.Unlock()
+	pc.entryMutex.RLock()
 	for i := 0; i < amount; i++ {
 		ret[i] = copyFun(pc.elements[i])
 	}
-	pc.mut.RUnlock()
+	pc.entryMutex.RUnlock()
 	return
-}
-
-func (pc *PrecomputedCache[MapKey, ElementType]) Validate() {
-	pc.mut.RLock()
-	if pc.createRandFromSeed == nil {
-		panic("Function to create rng seed uninitialized")
-	}
-	if pc.creationFun == nil {
-		panic("Function to create elements unintialized")
-	}
-	pc.mut.RUnlock()
 }
---
+++ internal/testutils/benchmark_meta_test.go
@@ -0,0 +1,52 @@
+package testutils
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/GottfriedHerold/Bandersnatch/internal/utils"
+)
+
+// TODO: Test thread-safety?
+
+var testPrecomputedCache = MakePrecomputedCache[int64, uint64](
+	func(key int64) *rand.Rand {
+		return rand.New(rand.NewSource(key))
+	},
+	func(rng *rand.Rand, key int64) uint64 {
+		return rng.Uint64()
+	},
+	func(in uint64) uint64 {
+		return in
+	},
+)
+
+func TestRetrievePrecomputedData(t *testing.T) {
+	const key1 = 10
+	const key2 = 11
+	data1 := testPrecomputedCache.GetElements(key1, 0)
+	FatalUnless(t, data1 != nil, "nil returned")
+	FatalUnless(t, len(data1) == 0, "invalid length")
+	data21 := testPrecomputedCache.GetElements(key2, 100)
+	data22 := testPrecomputedCache.GetElements(key2, 100)
+	FatalUnless(t, data21 != nil, "nil returned")
+	FatalUnless(t, data22 != nil, "nil returned")
+	FatalUnless(t, len(data21) == 100, "invalid length")
+	FatalUnless(t, len(data22) == 100, "invalid length")
+	FatalUnless(t, &data21[0] != &data22[0], "aliasing")
+	FatalUnless(t, utils.CompareSlices(data21, data22), "SlicesUnequal")
+	data23 := testPrecomputedCache.GetElements(key2, 50)
+	data24 := testPrecomputedCache.GetElements(key2, 200)
+	FatalUnless(t, data23 != nil, "nil returned")
+	FatalUnless(t, data24 != nil, "nil returned")
+	FatalUnless(t, len(data23) == 50, "invalid length")
+	FatalUnless(t, len(data24) == 200, "invalid length")
+	FatalUnless(t, utils.CompareSlices(data23, data24[0:50]), "No prefix")
+	// fmt.Println(data24)  -- manually insepct that it "looks random enough"
+
+	FatalUnless(t, CheckPanic(func() { testPrecomputedCache.PrePopulateCache(key2, []uint64{}) }), "did not panic")
+	testPrecomputedCache.PrePopulateCache(key1, []uint64{1, 2, 3})
+	data3 := testPrecomputedCache.GetElements(key1, 4)
+	FatalUnless(t, utils.CompareSlices(data3[0:3], []uint64{1, 2, 3}), "Did not get back prepopulated data")
+
+}
