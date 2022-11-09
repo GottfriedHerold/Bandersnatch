288e9b5ec49cce1318c6942bc75166278dd410d4
Gottfried Herold <gottfried.herold@rub.de>
2022-11-09 18:47:10 +0100

    Cleanup.

--- bandersnatch/fieldElements/bench_uint256_modular_test.go
+++ bandersnatch/fieldElements/bench_uint256_modular_test.go
@@ -21,17 +21,6 @@ func Benchmark_uint256_Modular(b *testing.B) {
 	b.Run("SquareEq_a (Barret)", benchmark_Copy_SquareEqBarret_a)
 	b.Run("LongMul256->512", benchmark_LongMul)
 	b.Run("LongSquare256->512", benchmark_LongSquare)
-
-	/*
-		b.Run("Neg", benchmarkNegEq)
-		b.Run("Double", benchmarkDoubleEq)
-		b.Run("Sub", benchmarkSubEq_ReduceWeak)
-		b.Run("Add", benchmarkAddEq_ReduceWeak)
-
-		b.Run("Square", benchmarkSquareEq)
-		b.Run("Multiply", benchmarkMulEq)
-		// b.Run("Invert", benchmarkInv)
-	*/
 }
 
 var (
@@ -243,88 +232,3 @@ func benchmark_LongSquare(b *testing.B) {
 		DumpUint512[n%benchS].LongSquare(&bench_x[n%benchS])
 	}
 }
-
-// Luan's old benchmarking code.
-
-/*
-func benchmarkAddEq_ReduceWeak(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.AddEqAndReduce_a(&y)
-		y.AddEqAndReduce_a(&x)
-	}
-
-}
-
-func benchmarkSubEq_ReduceWeak(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.SubEqAndReduce_a(&y)
-		y.SubEqAndReduce_a(&x)
-	}
-}
-
-func benchmarkInv(b *testing.B) {
-	var a uint256
-	count := 0
-	//runs over the test values
-OL:
-	for {
-		for _, _a := range testval {
-			a = _a
-			a.ModularInverse_a(&a)
-			a.ModularInverse_a(&a)
-
-			count += 2
-
-			if count >= b.N {
-				break OL
-			}
-		}
-	}
-}
-
-func benchmarkMulEq(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.MulEqAndReduce_a(&y)
-		y.MulEqAndReduce_a(&x)
-	}
-}
-
-func benchmarkSquareEq(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.SquareEqAndReduce_a()
-		y.SquareEqAndReduce_a()
-	}
-}
-
-func benchmarkNegEq(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.ComputeModularNegative_Weak_f()
-		y.ComputeModularNegative_Weak_f()
-	}
-}
-
-func benchmarkDoubleEq(b *testing.B) {
-	x := uint256{257, 479, 487, 491}
-	y := uint256{997, 499, 503, 509}
-
-	for i := 0; i < b.N; i += 2 {
-		x.DoubleEqAndReduce_a()
-		y.DoubleEqAndReduce_a()
-	}
-}
-*/
