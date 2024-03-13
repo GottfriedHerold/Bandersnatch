a02bbb4aba0820cf578536b068792203e942f707
Gottfried Herold <gottfried.herold@rub.de>
2024-03-13 07:35:11 +0100

    Testing of Comparison functions.

--- bandersnatch/errorsWithData/comparisons_test.go
+++ bandersnatch/errorsWithData/comparisons_test.go
@@ -29,7 +29,7 @@ var (
 var last_comparsion_mutex sync.Mutex // mutex protecting the above
 
 // special value to indicate "no value". We cannot use nil for that, as any(nil) might appear naturally.
-var unset struct{}
+var unset = new(struct{ bool }) // we use a pointer to have a unique value. Note that struct{}{} == struct{}{} in Go.
 
 // helper function for equality checks in tests
 
@@ -98,8 +98,20 @@ func (x StructIsEqual_Val) IsEqual(y StructIsEqual_Val) bool {
 	return x.val == y.val
 }
 func (x *StructIsEqual_Ptr) IsEqual(y *StructIsEqual_Ptr) bool {
-	last_comparison_target = y
+	last_comparison_target = *y
 	last_comparison_id = 2
+	return x.val == y.val
+}
+
+func (x IncomparableType_Val) IsEqual(y IncomparableType_Val) bool {
+	last_comparison_target = y.StructIsEqual_Val
+	last_comparison_id = 3
+	return x.val == y.val
+}
+
+func (x *IncomparableType_Ptr) IsEqual(y *IncomparableType_Ptr) bool {
+	last_comparison_target = y.StructIsEqual_Ptr
+	last_comparison_id = 4
 	return x.val == y.val
 }
 
@@ -107,6 +119,48 @@ func withPanicResults(f EqualityComparisonFunction) func(any, any) (result bool,
 	return func(x, y any) (result bool, didPanic bool, panicValue any) {
 		didPanic, panicValue = testutils.CheckPanic2(func() { result = f(x, y) })
 		return
+	}
+}
+
+type I2_Val struct {
+	utils.MakeIncomparable
+	StructIsEqual_Val
+}
+
+type I2_Ptr struct {
+	utils.MakeIncomparable
+	StructIsEqual_Ptr
+}
+
+func (x I2_Val) IsEqual(y any) bool {
+	last_comparison_target = y
+	last_comparison_id = 5
+	switch y := y.(type) {
+	case I2_Val:
+		return x.val == y.val
+	case StructIsEqual_Val:
+		return x.val == y.val
+	case IncomparableType_Val:
+		return x.val == y.val
+	default:
+		panic(fmt.Errorf("I2_Val.IsEqual called with %v of type %T", y, y))
+	}
+}
+
+func (x *I2_Ptr) IsEqual(y any) bool {
+	last_comparison_id = 6
+	switch y := y.(type) {
+	case *I2_Ptr:
+		last_comparison_target = y.StructIsEqual_Ptr
+		return x.val == y.val
+	case *StructIsEqual_Ptr:
+		last_comparison_target = *y
+		return x.val == y.val
+	case *IncomparableType_Ptr:
+		last_comparison_target = y.StructIsEqual_Ptr
+		return x.val == y.val
+	default:
+		panic(fmt.Errorf("I2_Val.IsEqual called with %v of type %T", y, y))
 	}
 }
 
@@ -142,27 +196,130 @@ func TestDummy(t *testing.T) {
 
 var Comp_IsEqual2 = CustomComparisonMethod("IsEqual") // functionally equivalent to Comparison_IsEqual
 
+var expectPanic = new(struct{ bool })
+var noFlip = new(struct{ bool })
+
+type namedT[T any] struct {
+	val  T
+	name string
+}
+
+func withName[T any](x T, name string) namedT[T] {
+	return namedT[T]{val: x, name: name}
+}
+
+// makeCheckerForComparisonFunctions returns a function test_fun(x,y,results, target...) that is used to test all comparison functions fun among funsWithNames
+// It should be called as makeCheckerForComparisonFunctions(t, withName[EqualityComparisonFunction](fun, "name"), ...)
+// where "name" is the name of the function fun (this name is used in error messages). The generic parameter may be deducible and omitted, depending on how fun was defined.
+//
+// The returned test_fun(x,y, result, target...) checks for each comparison function that
+// fun(x,y) == results
+// if target gets parsed as follows:
+//   - If expectPanic is among targets, we expect fun(x,y) to panic, otherwise we don't
+//   - If noFlip is among targets, we don't check fun(y,x)
+//   - If any remaining optional arguments are present, they passed to assumeComparisonState.
+//   - expectPanic and/or noFlip must come before the arguments passed to assumeComparisonState
+//
+// Unless noFlip was set, all tests are repeated for fun(y,x), i.e. with order of arguments flipped, *except for the call to assumeComparisonState*
+//
+// The resulting test_fun must only be called if last_comparison_mutex is held.
+func makeCheckerForComparisonFunctions(t *testing.T, funsWithNames ...namedT[EqualityComparisonFunction]) func(any, any, bool, ...any) {
+	testutils.FatalUnless(t, unset != expectPanic, "")
+	return func(x any, y any, expectedResult bool, target ...any) {
+		var shouldPanic bool = false
+		var doNotFlip bool = false
+		for len(target) > 0 && (target[0] == expectPanic || target[0] == noFlip) {
+			switch target[0] {
+			case expectPanic:
+				shouldPanic = true
+			case noFlip:
+				doNotFlip = true
+			default:
+				panic("Cannot happen")
+			}
+			target = target[1:]
+
+		}
+
+		for _, namedFunDirect := range funsWithNames {
+			funDirect := namedFunDirect.val
+			name := namedFunDirect.name
+
+			funCatchPanic := withPanicResults(funDirect)
+			var res, didPanic bool
+
+			res, didPanic, _ = funCatchPanic(x, y)
+			if len(target) > 0 {
+				assumeComparisonState(t, target...)
+			}
+			testutils.FatalUnless(t, res == expectedResult,
+				"Comparison function %v, called with %v and %v did not produce the expected result: Got %v, expected %v. GotPanic: %v",
+				name, x, y, res, expectedResult, didPanic)
+			testutils.FatalUnless(t, didPanic == shouldPanic,
+				"Comparison function %v, called with %v and %v did not match expected panic behavior: Expected Panic: %v, GotPanic: %v",
+				name, x, y, shouldPanic, didPanic)
+
+			if doNotFlip {
+				return
+			}
+
+			res, didPanic, _ = funCatchPanic(y, x)
+			/*if len(target) > 0 {
+				assumeComparisonState(t, target...)
+			}
+			*/
+			testutils.FatalUnless(t, res == expectedResult,
+				"Comparison function %v, called with %v and %v did not produce the expected result: Got %v, expected %v. GotPanic: %v",
+				name, y, x, res, expectedResult, didPanic)
+			testutils.FatalUnless(t, didPanic == shouldPanic,
+				"Comparison function %v, called with %v and %v did not match expected panic behavior: Expected Panic: %v, GotPanic: %v",
+				name, y, x, shouldPanic, didPanic)
+
+		}
+	}
+}
+
 func TestComparisonIsEqual(t *testing.T) {
 	last_comparsion_mutex.Lock()
 	defer last_comparsion_mutex.Unlock()
 	clearComparsionState()
 
-	checkPair := func(x any, y any, expectedResult bool, target ...any) {
-		testutils.FatalUnless(t, Comparison_IsEqual(x, y) == expectedResult, "")
-		assumeComparisonState(t, target...)
-		testutils.FatalUnless(t, Comparison_IsEqual(y, x) == expectedResult, "")
-		assumeComparisonState(t, target...)
-		testutils.FatalUnless(t, Comp_IsEqual2(x, y) == expectedResult, "")
-		assumeComparisonState(t, target...)
-		testutils.FatalUnless(t, Comp_IsEqual2(y, x) == expectedResult, "")
-		assumeComparisonState(t, target...)
+	checkPair := makeCheckerForComparisonFunctions(t, withName[EqualityComparisonFunction](Comparison_IsEqual, "Comparison_IsEqual"), withName(Comp_IsEqual2, "Comp_IsEqual2"))
 
-	}
-	checkPair(4, 5, false, unset, unset)
-	checkPair(4, 4, true, unset, unset)
-	checkPair(nil, (*int)(nil), true, unset, unset)
-	checkPair(nil, new(int), false, unset, unset)
-	checkPair(nil, nil, true, unset, unset)
-	checkPair(new(int), new(int), false, unset, unset)
+	checkPair(4, 5, false)
+	checkPair(4, 4, true)
+	checkPair(nil, (*int)(nil), true)
+	checkPair(nil, new(int), false)
+	checkPair(nil, nil, true)
+	checkPair(new(int), new(int), false)
+	intPtr := new(int)
+	checkPair(intPtr, intPtr, true)
+
+	incompValue := IncomparableType{}
+
+	sVal := StructIsEqual_Val{val: 4}
+	sVal2 := StructIsEqual_Val{val: 5}
+	sVal3 := StructIsEqual_Val{val: 4}
+	pVal := StructIsEqual_Ptr{val: 4}
+	pVal2 := StructIsEqual_Ptr{val: 5}
+	pVal3 := StructIsEqual_Ptr{val: 4}
+
+	sValI := IncomparableType_Val{StructIsEqual_Val: StructIsEqual_Val{val: 4}}
+	sValI2 := IncomparableType_Val{StructIsEqual_Val: StructIsEqual_Val{val: 5}}
+	pValI := IncomparableType_Ptr{StructIsEqual_Ptr: StructIsEqual_Ptr{val: 4}}
+	pValI2 := IncomparableType_Ptr{StructIsEqual_Ptr: StructIsEqual_Ptr{val: 5}}
+
+	checkPair(incompValue, incompValue, false, expectPanic)
+	checkPair(sVal, sVal, true, sVal, 1)
+	checkPair(sVal, sVal2, false, sVal2, 1)
+	checkPair(sVal, sVal3, true, sVal3, 1)
+	checkPair(pVal, pVal, true, pVal, 2)
+	checkPair(pVal, pVal2, false, pVal2, 2)
+	checkPair(pVal, pVal3, true, pVal3, 2)
+
+	checkPair(sValI, sValI, true, sVal, 3)
+	checkPair(sValI, sValI2, false, sVal2, 3)
+	checkPair(pValI, pValI, true, pVal, 4)
+	checkPair(pValI, pValI2, false, pVal2, 4)
 
 }
